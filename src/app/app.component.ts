ad238b777b7771cc153ffecb0cfcd40a0f45ba5d
Nihal Shukla <nihalgdev002@gmail.com>
2021-10-28 16:09:07 +0530

    if else block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,12 @@ export class AppComponent {
   getevent(evt){
     console.log(evt);
   }
+  show = true;
+  ss="yes";
   disable = true;
   enableBox(){
     this.disable = this.disable==false?true:false;
+    this.show = this.show==true?false:true;
   }
+  
 }
